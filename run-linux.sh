442d54f970249c22ac28236dbf5dcd8c25f594b6
Dodas <ph005009004@gmail.com>
2025-07-08 13:38:15 -0300

    add run script to linux and windows

---
+++ run-linux.sh
@@ -0,0 +1,11 @@
+DIR="$(pwd)/grafana"
+
+if [ -d "$DIR" ]; then
+echo "Diretório $DIR existe."
+else
+echo "Criando diretório $DIR..."
+mkdir -p "$DIR"
+echo "Diretório criado!"
+fi
+
+docker compose up
---
+++ run-windows.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+set "DIR=%cd%\grafana"
+
+if exist "%DIR%" (
+    echo Diretório %DIR% existe.
+) else (
+    echo Criando o diretório %DIR%...
+    mkdir "%DIR%"
+    echo Diretório criado!
+)
+
+docker compose up
+
+endlocal
+pause
